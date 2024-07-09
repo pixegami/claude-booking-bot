8850b975c5258df96a62c7be49f4f6b7e5ee1a90
Jack (Pixegami) <pixegami@gmail.com>
2024-07-10 00:16:51 +1000

    feat: Add a helper function to map/call tools
    Add a very simple script that loads our tool schemas into Python memory, and also provides a helper function to map an LLM output to a specific tool, and execute that tool (with the input parameters).

---
+++ tools/tools.py
@@ -0,0 +1,34 @@
+import json
+from anthropic.types.tool_use_block import ToolUseBlock
+
+from tools.ask_user import ask_user
+from tools.book_table import book_table
+from tools.get_booking_slots import get_booking_slots
+
+
+def load_schema(schema_path: str) -> dict:
+    with open(schema_path, "r") as file:
+        schema = json.load(file)
+    return schema
+
+
+# Load each of the tool schemas.
+ask_user_schema = load_schema("tools/schema/ask_user.json")
+get_booking_slots_schema = load_schema("tools/schema/get_booking_slots.json")
+book_table_schema = load_schema("tools/schema/book_table.json")
+
+
+def use_tool(tool_use_content: ToolUseBlock) -> dict:
+    """Helper function to use the correct tool based on the tool_use_content."""
+
+    tools_map = {
+        "ask_user": ask_user,
+        "get_booking_slots": get_booking_slots,
+        "book_table": book_table,
+    }
+
+    tool_name = tool_use_content.name
+    tool_input = tool_use_content.input
+    tool_function = tools_map[tool_name]
+
+    return tool_function(**tool_input)
